3e955e585710336a6ac4a9630c1091202517692a
Carlos Encine <contato@devhousesolutions.com.br>
2019-07-15 09:32:53 -0300

    corrigindo commit anterior

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<section id="form" class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-6 left-form">
+            <div class="btn-container">
+                <a href="" class="btn btn-transparent">Voltar para o site</a>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-12 col-lg-6 right-form">
+            <div class="form-container">
+                <div class="img-custom">
+                    <img src="img/logo.png" alt="">
+                </div>
+
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-lg custom-control" placeholder="E-mail">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-lg custom-control" placeholder="Senha">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-rounded btn-login btn-block btn-lg">ACESSAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
